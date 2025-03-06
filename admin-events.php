fa4114279bfd97a6cf40d19321ffd208adfbe21e
j3zzzz <jeznooo@gmail.com>
2025-03-06 15:16:38 +0800

    jess 03-06 (fixed login process, fixed sidebar links

--- admin-events.php
+++ admin-events.php
@@ -36,11 +36,11 @@ if (!$result) {
 <div class="container">
     <div class="sidebar">
         <div class="menu">
-            <a href="dashboard-admin.php" class="active"><i class="fas fa-home mr-3"></i>Home</a>
-            <a href="events-admin.php"><i class="fas fa-calendar-alt mr-3"></i>Events</a>
-            <a href="users-admin.php"><i class="fas fa-users mr-3"></i>Users</a>
-            <a href="notif-admin.php"><i class="fas fa-bell mr-3"></i>Notification</a>
-            <a href="profile-admin.php"><i class="fas fa-user-circle mr-3"></i>Profile</a>
+            <a href="admin-dashboard.php" class="active"><i class="fas fa-home mr-3"></i>Home</a>
+            <a href="admin-events.php"><i class="fas fa-calendar-alt mr-3"></i>Events</a>
+            <a href="admin-users.php"><i class="fas fa-users mr-3"></i>Users</a>
+            <a href="admin-notif.php"><i class="fas fa-bell mr-3"></i>Notification</a>
+            <a href="admin-profile.php"><i class="fas fa-user-circle mr-3"></i>Profile</a>
         </div>
     </div>
 
