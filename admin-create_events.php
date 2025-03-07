85c9ab0ba1d64db8176aaf2deb7561b70b0c41bd
Alessandra0319 <alessandra.castillas@cvsu.edu.ph>
2025-03-07 10:52:35 +0800

    alex

--- admin-create_events.php
+++ admin-create_events.php
@@ -512,12 +512,10 @@
         <!-- Sidebar -->
         <div class="sidebar">
             <div class="menu">
-                <a href="dashboard-admin.php"><i class="fas fa-home mr-3"></i>Home</a>
-                <a href="events-admin.php" class="active" ><i class="fas fa-calendar-alt mr-3"></i>Events</a>
-                <a href="users-admin.php"><i class="fas fa-users mr-3"></i>Users</a>
-                <a href="notif-admin.php"><i class="fas fa-bell mr-3"></i>Notification</a> 
-                <br><br><br><br><br><br><br><br><br><br><br><br><br>
-                <a href="progile-admin.php"><i class="fas fa-user-circle mr-3"></i>Profile</a>
+                <a href="admin-dashboard.php"><i class="fas fa-home mr-3"></i>Home</a>
+                <a href="admin-events.php" class="active" ><i class="fas fa-calendar-alt mr-3"></i>Events</a>
+                <a href="admin-users.php"><i class="fas fa-users mr-3"></i>Users</a>
+                <a href="admin-notif.php"><i class="fas fa-bell mr-3"></i>Notification</a> 
             </div>
         </div>
 
