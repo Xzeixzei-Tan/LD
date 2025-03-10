cb8141aab9c6b278d14e40ca16f701870a4863e2
hersheyline29 <hersheynhadz@gmail.com>
2025-03-10 13:54:37 +0800

    dudas 03-10

--- admin-archives.php
+++ admin-archives.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-        <link href="styles/admin-archives[.css" rel="stylesheet">
+        <link href="styles/admin-archives.css" rel="stylesheet">
         <title>admin-archives</title>
         <style>
             .content-area { display: flex; justify-content: space-between; }
