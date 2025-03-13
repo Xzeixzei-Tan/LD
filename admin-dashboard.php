1e08a8daa54925f7f7e05389354027ac2634bdee
ynnalrz <ynna.lrz03@gmail.com>
2025-03-13 14:46:00 +0800

    ynna 03-13

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -37,7 +37,7 @@ if (!$result) {
             <a href="admin-dashboard.php" class="active"><i class="fas fa-home"></i>Home</a>
             <a href="admin-events.php"><i class="fas fa-calendar-alt"></i>Events</a>
             <a href="admin-users.php"><i class="fas fa-users"></i>Users</a>
-            <a href="admin-notification.php"><i class="fas fa-bell"></i>Notification</a> 
+            <a href="admin-notif.php"><i class="fas fa-bell"></i>Notification</a> 
         </div>
     </div>
 
